8e8083084ffcf70290f274730af21846061d6a14
ya27hw <yousuf5550@gmail.com>
2026-05-27 05:36:14 +0400

    Add 2 day buffer

--- src/nyaa/utils.ts
+++ src/nyaa/utils.ts
@@ -5,6 +5,15 @@ import anilist from "@ani/anilist";
 import { excludeReleaseGroups } from "profile.json";
 
 const pageNumberLimit: number = 25;
+const AIRDATE_BUFFER_SECONDS = 2 * 24 * 60 * 60;
+
+function matchesAirdateWithBuffer(
+  airingAt: number,
+  pubEpoch: number,
+  bufferSeconds: number = AIRDATE_BUFFER_SECONDS
+): boolean {
+  return airingAt - bufferSeconds < pubEpoch;
+}
 
 /**
  * Calculates the page number given an episode number
@@ -180,7 +189,11 @@ function verifyQuery(
 
       const airDateMatch =
         ignoreAirdateChecks ||
-        (pageNumber !== -1 && airDates.nodes[pageNumber].airingAt < pubEpoch);
+        (pageNumber !== -1 &&
+          matchesAirdateWithBuffer(
+            airDates.nodes[pageNumber].airingAt,
+            pubEpoch
+          ));
 
       return (
         +episodeMatch +
@@ -205,7 +218,10 @@ function verifyQuery(
       const airDateMatchBatch =
         ignoreAirdateChecks ||
         (airDates.nodes.length > 0 &&
-          airDates.nodes[airDates.nodes.length - 1].airingAt < pubEpoch); // Check if the episode date is similar
+          matchesAirdateWithBuffer(
+            airDates.nodes[airDates.nodes.length - 1].airingAt,
+            pubEpoch
+          )); // Check if the episode date is similar
 
       const episodeRange = fileName.match(/\d+\s*[-~]\s*\d+/); // Check if the file name contains a range of episodes
       if (episodeRange)
